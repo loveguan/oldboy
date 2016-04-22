2b7ef992754fc1cba76fa896386b0df85cbcc376
loveguan <zhouguanjie@qq.com>
2016-04-22 11:24:43 +0800

    version 2

---
+++ test.py
@@ -0,0 +1,15 @@
+this_year = 2016
+name = raw_input('please input your name:')
+age =int( raw_input(('please input your age:')))
+sex = raw_input("please input your sex:")
+dep = raw_input("which department:")
+message = """Infornation of the company staff:
+        Name: %s
+        Age : %d
+        Sex : %s
+        Dep : %s
+""" %(name,age,sex,dep)
+print message
+print "Hello,", name,'\n'
+print "you are", age, 'years'
+print "so you were born in: ", this_year - age
